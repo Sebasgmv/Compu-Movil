ecc732a2c5db56ea8c682a51b6cb4b0daa61a699
Naruto360XL <sebastian@hotmail.com>
2022-09-25 14:56:49 -0500

    Binding

--- Taller 2/app/src/main/java/puj/movil/myapplication/MainActivity.java
+++ Taller 2/app/src/main/java/puj/movil/myapplication/MainActivity.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import puj.movil.myapplication.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    //Ni idea pq sale
+    public ActivityMainBinding getBinding() {
+        return binding;
     }
 }
